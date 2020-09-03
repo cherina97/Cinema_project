26aa3652f39b13ffd0c615739e9d03239994c3de
cherina97 <cherina97@gmail.com>
2020-09-03 19:00:21 +0300

    add main fields, getters/setters

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Cinema {
+
+    private Map<Days, Schedule> schedules = new TreeMap<>();
+    private List<Movie> movies = new ArrayList<>();
+    private Time timeOpen;
+    private Time timeClose;
+
+    public Map<Days, Schedule> getSchedules() {
+        return schedules;
+    }
+
+    public void setSchedules(Map<Days, Schedule> schedules) {
+        this.schedules = schedules;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Time getTimeOpen() {
+        return timeOpen;
+    }
+
+    public void setTimeOpen(Time timeOpen) {
+        this.timeOpen = timeOpen;
+    }
+
+    public Time getTimeClose() {
+        return timeClose;
+    }
+
+    public void setTimeClose(Time timeClose) {
+        this.timeClose = timeClose;
+    }
 }
--- src/Time.java
+++ src/Time.java
@@ -6,14 +6,28 @@ public class Time {
     private int hour;
 
     public Time(int min, int hour) throws IllegalTimeFormatException {
-        if ((min < 0 || min > 60) && (hour < 0 || hour > 24)){
-            throw new IllegalTimeFormatException("Mistake! Time must be between 0-60 for minutes and 0-24 for hours");
+        if ((min < 0 || min > 60) && (hour < 0 || hour > 24)) {
+            throw new IllegalTimeFormatException("Mistake! Time must be between 0-60 for minutes and 0-24 for hours!");
         } else {
             //todo this.setMin(min);
             //     this.setHour(hour);
             this.min = min;
             this.hour = hour;
         }
+    }
+
+    public static Time endTime(Time startTime, Time duration) throws IllegalTimeFormatException {
+        int min = startTime.getMin() + duration.getMin();
+        int hour = startTime.getHour() + duration.getHour();
+
+        if (min > 60) {
+            hour++;
+            min = 0;
+        } else if (hour > 24) {
+            hour = 1;
+        }
+
+        return new Time(min, hour);
     }
 
     public int getMin() {
